265d01b6dc55f722e549768eb76626df88d37c34
premkumar143341 <22pa1a0567@vishnu.edu.in>
2025-06-25 09:52:11 +0530

    Create 3.rs

---
+++ 3.rs
@@ -0,0 +1,22 @@
+/*Write a program to do the following
+a. Declare a variable x and store value 1000 in it.
+b. Declare a variable y and store value “Programming” in it
+c. Print the values of x and y
+d. Change the value of x to 1100
+e. Print the values of x and y*/
+fn main() {
+    // a. Declare a variable x and store value 1000 in it.
+    let mut x = 1000;
+
+    // b. Declare a variable y and store value "Programming" in it.
+    let y = "Programming";
+
+    // c. Print the values of x and y
+    println!("x = {}, y = {}", x, y);
+
+    // d. Change the value of x to 1100
+    x = 1100;
+
+    // e. Print the values of x and y
+    println!("x = {}, y = {}", x, y);
+}
